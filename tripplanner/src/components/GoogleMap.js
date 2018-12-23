f4d4388c0f0f6f50280af62b8b6eb4bf7c9a284a
RaymondChoi54 <concept_ray@hotmail.com>
2018-12-23 18:46:58 -0500

    Hiding google key

--- tripplanner/src/components/GoogleMap.js
+++ tripplanner/src/components/GoogleMap.js
@@ -1,30 +1,47 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import redDot from'../redDot.png';
+import redDot from '../redDot.png';
+import config from '../config'
+
+const defaultProps = {
+    center: {
+    	lat: 40.74215837520133,
+    	lng: -73.98990542164569
+    },
+    zoom: 12.5
+};
 
 const AnyReactComponent = ({ text }) => <img src={redDot} alt={text} width="18" height="18" />;
 
 const ContainMap = ({ state, onSelectLocation }) => {
-	const defaultProps = {
-	    center: {
-	    	lat: 40.74215837520133,
-	    	lng: -73.98990542164569
-	    },
-	    zoom: 12.5
-  	};
 
   	if(state.selection) {
+  		let latSel = state.latitude
+  		let lngSel = state.longitude
+  		console.log(state);
+  		console.log(state.latitude)
+  		console.log(state.longitude)
   		return (
 			<div style={{ height: '500px', width: '500px' }}>
 				<GoogleMapReact
-					bootstrapURLKeys={{ key: 'AIzaSyCl3R6ucF5xAzTqSgAzMY55DtoK_wWUZzY'}}
+					bootstrapURLKeys={{ key: config.key}}
 					defaultCenter={defaultProps.center}
 					defaultZoom={defaultProps.zoom}
 					onClick={location => onSelectLocation(location.lat, location.lng)}
 				>
-					<AnyReactComponent
+	          		<AnyReactComponent
 	          			lat={state.latitude}
 	          			lng={state.longitude}
+	          			text="sfdgsdfg"
+	          		/>
+					<AnyReactComponent
+	          			lat={latSel}
+	          			lng={lngSel}
+	          			text="sfdgsdfg"
+	          		/>
+	          		<AnyReactComponent
+	          			lat={40.74215837520133}
+	          			lng={-73.98990542164569}
 	          			text="sfdgsdfg"
 	          		/>
 		          	{state.locations.map((location, index) => (
@@ -36,14 +53,13 @@ const ContainMap = ({ state, onSelectLocation }) => {
 		          		/>
 		          	))}
 				</GoogleMapReact>
-				{"sdasdgasg"}
 			</div>
 		)
   	} else {
 		return (
 			<div style={{ height: '500px', width: '500px' }}>
 				<GoogleMapReact
-					bootstrapURLKeys={{ key: 'AIzaSyCl3R6ucF5xAzTqSgAzMY55DtoK_wWUZzY'}}
+					bootstrapURLKeys={{ key: config.key}}
 					defaultCenter={defaultProps.center}
 					defaultZoom={defaultProps.zoom}
 					onClick={location => onSelectLocation(location.lat, location.lng)}
